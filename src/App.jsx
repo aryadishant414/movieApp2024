db9307f5da243ee76907c79391dab452a7f381f5
dishant <aryadishant414@gmail.com>
2024-11-21 09:49:02 +0530

    Completed movie app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourite from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [favourites, setFavourites] = useState([]);
 
       const getMovieRequest = async () => {
-        const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
+
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+
         const response = await fetch(url);
         const responseJson = await response.json();
+
         console.log("Data inside External Api Response is : ", responseJson);
 
         if (responseJson.Search) {
@@ -17,15 +26,58 @@ const App = () => {
         }
     };
 
+    const saveToLocalStorage = (items) => {
+      localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));  // .json.stringify -> converts our javascript object into JSON format
+    };
+
+    const addFavouriteMovie = (movie) => {
+      const newFavouriteList = [...favourites, movie];
+      setFavourites(newFavouriteList);
+      saveToLocalStorage(newFavouriteList);
+    };
+
+    const removeFavouriteMovie = (movie) => {
+      const newFavouriteList = favourites.filter(
+          (favourite) => favourite.imdbID !== movie.imdbID
+      );
+
+      setFavourites(newFavouriteList);
+      saveToLocalStorage(newFavouriteList);
+    };
+
       useEffect(() => {
         getMovieRequest();
-      } , [])
+      } , [searchValue]);
+
+      useEffect(() => {
+        const movieFavourites = JSON.parse(       // .json.parse -> converting back into our javascript object
+            localStorage.getItem('react-movie-app-favourites')
+        );
+
+        setFavourites(movieFavourites);
+      }, []);
+
+
+      
 
   return (
       <div className='container-fluid movie-app'>
-          <div className='row'>
-            <MovieList movies={movies} />
-          </div>
+        <div className='row d-flex align-items-center mt-4 mb-4'>
+          <MovieListHeading heading="Movies" />
+          <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+        </div>
+
+        <div className='overflow-y-hidden d-flex border'>   {/*removed ROW class from here*/} 
+          <MovieList movies={movies} favouriteComponent={AddFavourite} handleFavouritesClick={addFavouriteMovie} />
+        </div>
+
+        <div className='row d-flex align-items-center mt-4 mb-4'>
+          <MovieListHeading heading="Favourites" />
+        </div>
+
+        <div className='overflow-y-hidden d-flex border'>     {/*removed ROW class from here*/} 
+            <MovieList movies={favourites} favouriteComponent={RemoveFavourites} handleFavouritesClick={removeFavouriteMovie} />
+        </div>
       </div>
   );
 };
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 const MovieList = (props) => {
     // console.log("INSIDE MOVIE LIST PROPS IS : ", props); // just to check
+    const FavouriteComponent = props.favouriteComponent;
     return (
         <>
             {props.movies.map((movie, index) => (
                 
-                <div className="col-sm-4">
-                    <div className='image-container d-flex justify-content-start m-3'>
-                        <img src={movie.Poster} alt='movie'></img>
-                    </div>
+                <div className='image-container d-flex  justify-content-start m-3'>
+                    
+                    <img src={movie.Poster} alt='movie'></img>
+                    <span onClick={() => props.handleFavouritesClick(movie)} className='overlay d-flex align-items-center justify-content-center'>
+                        <FavouriteComponent />
+                    </span>
                 </div>
                 
             ))}
